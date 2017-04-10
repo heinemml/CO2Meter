57d0bb5d3b280dbf53c12e1becadcb9eaf8e8791
Michael Heinemann <michael.heinemann@iba-group.com>
2017-04-10 13:40:36 +0200

    bump version to 2.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='CO2Meter',
-      version='2.0',
+      version='2.1',
       py_modules=['CO2Meter'],
       url='https://github.com/heinemml/CO2Meter',
       description='Library to access USB CO2Meters'
