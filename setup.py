342d1952cdcb93688a9443abd3292083a3c803f2
Michael Nosthoff <git@stresser.de>
2020-02-25 10:51:57 +0100

    bump version to 2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='CO2Meter',
-      version='2.1',
+      version='2.2',
       py_modules=['CO2Meter'],
       url='https://github.com/heinemml/CO2Meter',
       description='Library to access USB CO2Meters'
